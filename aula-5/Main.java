1efde73ef61c175fe42462bbd06467d8086348a8
aira922 <aira.s.m.bonke@gmail.com>
2025-06-14 10:36:12 -0300

    Update Main.java

--- aula-5/Main.java
+++ aula-5/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		int maiorNumero=0;
+		int menorNumero =Integer.MAX_VALUE;
+		int vetor [] = new int[10];
+		for (int i = 0; i<10; i++) {
+			vetor[i]= sc.nextInt();
+		}
+		for (int  i = 0; i<10; i++) {
+			if (vetor[i]>maiorNumero) {
+				maiorNumero =vetor[i] ;
+			}
+		}
+		for (int  i = 0; i<10; i++) {
+			if (vetor[i]<menorNumero) {
+				menorNumero=vetor[i]   ;
+			}
+		}
+			System.out.println("Maior: "+maiorNumero);
+			System.out.println("Menor: "+menorNumero);
 	}
 }
